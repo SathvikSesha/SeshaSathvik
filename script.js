c4b514bdd129167309e69025304783fd72855b9c
SathvikSesha <lakshmisarmatk@gmail.com>
2025-05-24 10:52:44 +0530

    Js file

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const tabLinks = document.querySelectorAll(".tab-links");
 const tabContents = document.querySelectorAll(".tab-contents");
 
+const nam = document.querySelector("#name");
+
 tabLinks.forEach((link, index) => {
   link.addEventListener("click", () => {
     tabLinks.forEach((link) => link.classList.remove("active-link"));
@@ -12,3 +14,19 @@ tabLinks.forEach((link, index) => {
     tabContents[index].classList.add("active-tab");
   });
 });
+
+function animateName() {
+  nam.animate(
+    [
+      { transform: "translate(0px,0px)" },
+      { transform: "translate(-2px,-2px)" },
+      { transform: "translate(0px,0px)" },
+    ],
+    {
+      duration: 2000,
+      iterations: Infinity,
+    }
+  );
+}
+
+window.addEventListener("DOMContentLoaded", animateName);
